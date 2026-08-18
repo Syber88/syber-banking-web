c06d54e2fedd3be07ae3bbcfd37c40c17d536338
Siyabonga Magwaza <ssmagwaza88@gmail.com>
2026-08-18 11:44:45 +0200

    done with navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,29 +1,58 @@
 import { useState } from "react";
 
 function Navbar() {
+  const [underline, setUnderline] = useState("Overview");
+
   return (
     <nav className="syber-navbar">
-
       {/*Left Side*/}
       <div className="syber-navbar-left">
-      < div className="profile-circle">
-        S
+        <div className="profile-circle">S</div>
+
+        <a className="navbar-brand" href="#">
+          Syber
+        </a>
       </div>
-    
-      <a className="navbar-brand" href="#">
-        Syber
-      </a>
+
+      {/**Right Side */}
+      <div className="syber-navbar-right">
+        <div className="profile-circle-right">
+          SM
+        </div>
       </div>
 
       {/**Navigation buttons */}
       <div className="navbar-buttons">
-        <button>Overview</button>
-        <button>Accounts</button>
-        <button>Transactions</button>
-        <button>Transfer</button>
+        <button 
+        className={underline === "Overview" ? "active" : ""}
+        onClick={() =>setUnderline("Overview")}
+        >
+          Overview
+        </button>
+
+        <button 
+        className={underline === "Accounts" ? "active" : ""}
+        onClick={() =>setUnderline("Accounts")}
+        >
+          Accounts
+        </button>
+
+        <button 
+        className={underline === "Transactions" ? "active" : ""}
+        onClick={() =>setUnderline("Transactions")}
+          >
+          Transactions
+        </button>
+
+        <button 
+        className={underline === "Transfer" ? "active" : ""}
+        onClick={() =>setUnderline("Transfer")}
+          >
+          Transfer
+        </button>
+
       </div>
     </nav>
-
   );
 }
 
